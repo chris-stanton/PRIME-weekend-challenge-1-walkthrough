940349e60c73f4c7c7e6ea20719a72f2d0c88e7d
Chris Stanton <chrisstanton@Chriss-MacBook-Pro.local>
2017-02-13 11:09:06 -0600

    added delete button

--- client.js
+++ client.js
@@ -1,5 +1,36 @@
 
 $(document).ready(function() {
+  $('#submitNewEmployee').on('click', function(){
 
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var idNumber = $('#idNumber').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
+// .append() needs a string value
+// + joins merges content to a string
+    $('#employeeTableBody').append(
+// <tr> is now in a string form that includes all <td>
+// Adds new employee to the DOM
+    '<tr>' +
+      '<td>' + firstName + '</td>' +
+      '<td>' + lastName + '</td>' +
+      '<td>' + idNumber + '</td>' +
+      '<td>' + jobTitle + '</td>' +
+      '<td>' + annualSalary + '</td>' +
+      '<td><button class="deleteEmployeeButton">Delete ' + firstName + '</button></td>' +
+    '</tr>'
+    );
+// Add monthly salary to the DOM
+  var newEmployeeMonthlyExpenses = annualSalary / 12;
+//$('#monthlyExpenses').text(); pulls value from line and converts it to a string
+  var perviousSalaryTotal = $('#monthlyExpenses').text();
+  var totalMonthlyExpenses = parseFloat(perviousSalaryTotal) + newEmployeeMonthlyExpenses;
+  $('#monthlyExpenses').text(totalMonthlyExpenses);
+});
+
+  $('#employeeTableBody').on('click', '.deleteEmployeeButton', function(){
+    $(this).parent().parent().remove();
+});
 
 });
